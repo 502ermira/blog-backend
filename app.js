8274192990774d6209184f0a040448cd30bcebb3
502ermira <ermiraaa20@gmail.com>
2024-05-27 23:31:04 +0200

    Add CORS support to Express server

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const blogRoutes = require('./routes/blogs');
 const newsletterRoutes = require('./routes/newsletter');
 
@@ -8,6 +9,8 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.use(express.json());
 
