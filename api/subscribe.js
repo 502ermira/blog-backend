c370b05085d41bc3b97575aac4b857010cc027b8
502ermira <ermiraaa20@gmail.com>
2024-05-28 16:11:24 +0200

    Add CORS headers to newsletter subscription endpoint

--- api/subscribe.js
+++ api/subscribe.js
@@ -9,10 +9,9 @@ mongoose.connect(process.env.MONGO_URI, {
   .catch(err => console.error(err));
 
 module.exports = async (req, res) => {
-
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Origin', '*'); 
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS'); 
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); 
 
   if (req.method === 'OPTIONS') {
     res.status(200).end();
