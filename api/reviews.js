29e9c4d6475b9514a40f0e9e27b934605b897f32
502ermira <ermiraaa20@gmail.com>
2024-05-31 17:56:40 +0200

    Configure reviews API with GET and POST handlers

--- api/reviews.js
+++ api/reviews.js
@@ -34,6 +34,28 @@ app.post('/reviews', authenticateJWT, async (req, res) => {
   }
 });
 
+const postReview = async (req, res) => {
+    console.log('User in request:', req.user);
+  
+    const { rating, review } = req.body;
+    const newReview = new Review({
+      user: req.user._id,
+      name: req.user.name,
+      email: req.user.email,
+      picture: req.user.picture,
+      rating,
+      review
+    });
+  
+    try {
+      await newReview.save();
+      res.status(201).json(newReview);
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  };
+  
+
 app.get('/reviews', async (req, res) => {
   try {
     const reviews = await Review.find().populate('user').exec();
@@ -43,4 +65,17 @@ app.get('/reviews', async (req, res) => {
   }
 });
 
-module.exports = app;
+const getReviews = async (req, res) => {
+    try {
+      const reviews = await Review.find().populate('user').exec();
+      res.json(reviews);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  };
+
+  module.exports = {
+    getReviews,
+    postReview
+  };
+  module.exports = router;
